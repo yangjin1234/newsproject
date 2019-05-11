78bfb61a363d134ef11a8cb2de0b8718b7506461
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-11 18:56:39 +0800

    在loginaction里面添加了内容

--- src/web/action/LoginAction.java
+++ src/web/action/LoginAction.java
@@ -3,6 +3,9 @@ package web.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.LoginService;
+import service.impl.LoginServiceImpl;
+import util.MyLog;
 import web.core.Action;
 import web.core.ActionForm;
 import web.core.ActionForward;
@@ -12,8 +15,16 @@ public class LoginAction extends Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response, ActionForm af) {
-		// TODO Auto-generated method stub
-		return null;
+		String lname=request.getParameter("lname");
+		String lpass=request.getParameter("lpass");
+		LoginService ls=new LoginServiceImpl();
+		int state=ls.Login(lname, lpass);
+		MyLog.log.debug("state="+state);
+		if(state==1){
+			return new ActionForward("show");
+		}else {
+			return new ActionForward(true,"login");
+		}
 	}
 
 }

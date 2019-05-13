febcb7ccd8ceb1e8b6e18a25407000cdb783cdf4
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-13 09:16:17 +0800

    在loginaction中做了改动  改成接收jQuery的Ajax传送的数据

--- src/web/action/LoginAction.java
+++ src/web/action/LoginAction.java
@@ -1,29 +1,55 @@
 package web.action;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import pojo.Login;
+import net.sf.json.JSONObject;
 import service.LoginService;
 import service.impl.LoginServiceImpl;
 import util.MyLog;
 import web.core.Action;
 import web.core.ActionForm;
 import web.core.ActionForward;
+import web.form.LoginForm;
 
 public class LoginAction extends Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response, ActionForm af) {
-		String lname=request.getParameter("lname");
-		String lpass=request.getParameter("lpass");
+//		String lname=request.getParameter("lname");
+//		String lpass=request.getParameter("lpass");
+//		LoginService ls=new LoginServiceImpl();
+//		int state=ls.Login(lname, lpass);
+//		MyLog.log.debug("state="+state);
+//		if(state==1){
+//			return new ActionForward("show");
+//		}else {
+//			return new ActionForward(true,"loginshow");
+//		}
+		LoginForm loginf=(LoginForm)af;
+		JSONObject obj = JSONObject.fromObject(loginf.getData());
+		MyLog.log.debug("state="+obj);
+		Login l=(Login)JSONObject.toBean(obj);
 		LoginService ls=new LoginServiceImpl();
-		int state=ls.Login(lname, lpass);
-		MyLog.log.debug("state="+state);
+		int state=ls.Login(l.getLname(), l.getLpass());
 		if(state==1){
 			return new ActionForward("show");
 		}else {
-			return new ActionForward(true,"loginshow");
+			JSONObject returnval=JSONObject.fromObject(state);
+			try {
+				PrintWriter out=response.getWriter();
+				out.write(returnval.toString());
+				out.flush();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
 		}
 	}
 

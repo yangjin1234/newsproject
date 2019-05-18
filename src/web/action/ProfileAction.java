e75cdca6883c742553d55128a1e7d2854a86de29
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-18 20:09:04 +0800

    修改了profileaction

--- src/web/action/ProfileAction.java
+++ src/web/action/ProfileAction.java
@@ -3,6 +3,7 @@ package web.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import util.MyLog;
 import web.core.Action;
 import web.core.ActionForm;
 import web.core.ActionForward;
@@ -12,8 +13,11 @@ public class ProfileAction extends Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response, ActionForm af) {
-		// TODO Auto-generated method stub
-		return null;
+		String nickname=request.getParameter("nickname");
+		MyLog.log.debug("nickname="+nickname);
+		MyLog.log.debug("nickname="+nickname);
+		request.getSession().setAttribute("nickname", nickname);
+		return new ActionForward("profile");
 	}
 
 }

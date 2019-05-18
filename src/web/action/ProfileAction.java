be285eb8cd06c10a1282c88e871be7e34ed3dc55
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-18 17:52:08 +0800

    配置文件添加信息 建立profileAction和ProfileForm

---
+++ src/web/action/ProfileAction.java
@@ -0,0 +1,19 @@
+package web.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web.core.Action;
+import web.core.ActionForm;
+import web.core.ActionForward;
+
+public class ProfileAction extends Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response, ActionForm af) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

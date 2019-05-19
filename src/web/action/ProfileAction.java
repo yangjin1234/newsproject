e18ef64ab06b19d53e0a8ee2f523767dc63423c3
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-19 22:26:48 +0800

    profileaction做了修改

--- src/web/action/ProfileAction.java
+++ src/web/action/ProfileAction.java
@@ -3,6 +3,10 @@ package web.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import pojo.impl.LoginImpl;
+import pojo.impl.Userinformation;
+import service.UserInformationService;
+import service.impl.UserInformationServiceImpl;
 import util.MyLog;
 import web.core.Action;
 import web.core.ActionForm;
@@ -13,16 +17,29 @@ public class ProfileAction extends Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request,
 			HttpServletResponse response, ActionForm af) {
-		String uname=request.getParameter("user_name");
-		MyLog.log.debug("user_name="+uname);
-		String usex=request.getParameter("user_sex");
-		MyLog.log.debug("user_sex="+usex);
-		String email=request.getParameter("email");
-		String mobile=request.getParameter("mobile");
-		request.getSession().setAttribute("uname", uname);
-		request.getSession().setAttribute("email", email);
-		request.getSession().setAttribute("mobile", mobile);
-		return new ActionForward("profileweb");
+		//得到登录用户
+				LoginImpl log=new LoginImpl();
+				 log=(LoginImpl)request.getSession().getAttribute("account");
+				 String uname=log.getLname();
+				 UserInformationService uis=new UserInformationServiceImpl();
+				 Userinformation useri=uis.selectUserByName(uname);
+				 String usex=useri.getUsex();
+				 String uemail=useri.getUemail();
+				 String uphone=useri.getUphone();
+				 String uphoto=useri.getUphoto();
+				 uname=request.getParameter("user_name");
+				MyLog.log.debug("user_name="+uname);
+				usex=request.getParameter("user_sex");
+				MyLog.log.debug("user_sex="+usex);
+				uemail=request.getParameter("email");
+				uphone=request.getParameter("mobile");
+				uphoto=request.getParameter("photo");
+				
+				request.getSession().setAttribute("uname", uname);
+				request.getSession().setAttribute("uemail", uemail);
+				request.getSession().setAttribute("umobile", uphone);
+				request.getSession().setAttribute("uphoto", uphoto);
+				return new ActionForward("profileweb");
 	}
 
 }

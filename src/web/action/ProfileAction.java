c0c625451ecdf0fc732287eb7eb303ac2e860fd9
Rasery <Rasery@DESKTOP-L2OHASL>
2019-05-23 18:59:14 +0800

    删除了无效图片

--- src/web/action/ProfileAction.java
+++ src/web/action/ProfileAction.java
@@ -1,9 +1,7 @@
 package web.action;
 
-import java.io.FileOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +11,6 @@ import pojo.impl.Userinformation;
 import service.UserInformationService;
 import service.impl.UserInformationServiceImpl;
 import util.MyLog;
-import web.core.Action;
 import web.core.ActionForm;
 import web.core.ActionForward;
 import web.core.DispatcherAction;
@@ -33,13 +30,13 @@ public class ProfileAction extends DispatcherAction{
 				 String uemail=useri.getUemail();
 				 String uphone=useri.getUphone();
 				 String uphoto=useri.getUphoto();
-				 request.getSession().setAttribute("usex", usex);
 				 MyLog.log.debug("user_sex="+usex);
 				 try {
 					usex = URLDecoder.decode(usex, "UTF-8");
 				} catch (UnsupportedEncodingException e) {
 					e.printStackTrace();
 				}
+				 request.getSession().setAttribute("usex", usex);
 				request.getSession().setAttribute("uname", uname);
 				request.getSession().setAttribute("uemail", uemail);
 				request.getSession().setAttribute("uphone", uphone);
